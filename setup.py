c3206071eca37a76b53947eef10ffdd5cc3141a9
Jorge Dias <jorge@mrdias.com>
2017-03-13 11:43:43 +0100

    Pyroma suggestions

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name='bless_deployer',
     version='0.0.1',
-    description='Deploy Bless to AWS',
+    description='Create configurations and deploy Bless to AWS',
     author='Jorge Dias',
     author_email='jorge@mrdias.com',
-    url='',
+    url='https://github.com/diasjorge/bless-deployer',
+    keywords=['AWS', 'Bless'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
     license="FreeBSD License",
     packages=find_packages(exclude=["test*", "data*"]),
     package_data={'bless_deployer': ['data/*.*']},
@@ -14,6 +25,7 @@ setup(
         'boto3',
         'configargparse'
     ],
+    zip_safe=False,
     extras_require={
         'dev': [
             'zest.releaser[recommended]'
